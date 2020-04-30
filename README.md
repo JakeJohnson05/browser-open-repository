ddfbb6a381635851255272bbff488bc9a6a818b6
Jake Johnson <jakejohnson801@outlook.com>
2020-04-30 15:57:13 -0600

    Updated tests

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # browser-open-repository
 
+[![Build Status](https://travis-ci.org/JakeJohnson05/browser-open-repository.svg?branch=master)](https://travis-ci.org/JakeJohnson05/browser-open-repository)
 [![License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](https://raw.githubusercontent.com/JakeJohnson05/browser-open-repository/master/LICENSE)
 
-Interactive cli tool that lists and run `package.json` scripts.
-
+Open your github repositories in your browser
 
 ## Table of Contents
 	* [Install](#arrow_down-install)
--- cli.js
+++ cli.js
@@ -1,13 +1,29 @@
-/*
-repo() {
-	alias repo="git remote -v 2>/dev/null | awk \"/^origin.*\(push\)$/ {print \\\$2}\" | xargs start chrome"
-alias repo="git remote -v 2>/dev/null | sed -rn \"s/^origin\s(.*)\s\(push\)$/\1/p\""
-	isGit || return 1;
-	local repoUrl=$(git remote -v 2>/dev/null | awk '/^origin\s.*\s\(push\)$/ {print $2}')
-	local repoUrl=$(git remote -v 2>/dev/null | awk '/^origin\s.*\s\(push\)$/ {print $2}')
-	echo "repoUrl: $repoUrl"
-	[ -z "$repoUrl" ] && { printErr "No repoURL found"; return 1; }
-	echo "Repo URL exists"
-}
-*/
-// "test": "xo && ava && tsd"
+#!/usr/bin/env node
+'use strict';
+
+require('inquirer-exit-listener');
+/** Loading animation when finding the projects */
+// const oraSpinner = require('ora')('Finding Projects...');
+// const { writeFile } = require('fs');
+// const { join } = require('path');
+// const inquirer = require('inquirer');
+// const { green, bold } = require('chalk');
+const {bold} = require('chalk');
+// Const logSymbols = require('log-symbols');
+// const table = require('text-table');
+const yargs = require('yargs/yargs')(process.argv.slice(2));
+const {argv} = yargs
+	.scriptName('openProject')
+	.wrap(Math.min(100, yargs.terminalWidth()))
+	.usage('')
+	.usage(bold('open-project'))
+	.usage('  Easily navigate to your project directories')
+	.usage('')
+	.usage('Usage:')
+	.usage('  $ openProject [options]')
+	.usage('Aliases: op')
+	.help()
+	.alias('h', 'help')
+	.epilog('Visit https://github.com/JakeJohnson05/OpenProjectCli for more info');
+
+console.log(argv);
